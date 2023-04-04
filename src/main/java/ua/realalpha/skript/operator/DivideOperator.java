86957eaaf88f3ac35ff947ab8c7c23b07a59e9a1
RealAlphaUA <realalpha.ua@gmail.com>
2023-04-05 01:25:46 +0200

    add Operators 🎀

---
+++ src/main/java/ua/realalpha/skript/operator/DivideOperator.java
@@ -0,0 +1,36 @@
+package ua.realalpha.skript.operator;
+
+import ua.realalpha.skript.token.Token;
+
+public class DivideOperator implements Operator {
+    @Override
+    public String getName() {
+        return "Divide";
+    }
+
+    @Override
+    public String getOperator() {
+        return "/";
+    }
+
+    @Override
+    public int getPriority() {
+        return 2;
+    }
+
+    @Override
+    public Object process(Token a, Token b) {
+        Number numberA = a.toNumber();
+        Number numberB = b.toNumber();
+
+        if (numberA instanceof Double || numberB instanceof Double) {
+            return numberA.doubleValue() / numberB.doubleValue();
+        } else if (numberA instanceof Float || numberB instanceof Float) {
+            return numberA.floatValue() / numberB.floatValue();
+        } else if (numberA instanceof Long || numberB instanceof Long) {
+            return numberA.longValue() / numberB.longValue();
+        } else {
+            return numberA.intValue() / numberB.intValue();
+        }
+    }
+}
---
+++ src/main/java/ua/realalpha/skript/operator/EqualsOperator.java
@@ -0,0 +1,32 @@
+package ua.realalpha.skript.operator;
+
+import ua.realalpha.skript.token.Token;
+
+public class EqualsOperator implements Operator {
+
+    @Override
+    public String getName() {
+        return "equals";
+    }
+
+    @Override
+    public String getOperator() {
+        return "==";
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+    @Override
+    public Object process(Token a, Token b) {
+
+        if (a.isNumber() && b.isNumber()) {
+            return a.toNumber().doubleValue() == b.toNumber().doubleValue();
+        }
+
+        return a.getVariable().equals(b.getVariable());
+    }
+
+}
