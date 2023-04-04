781bc7b21bc3048014e22041c058076352805ac0
RealAlphaUA <realalpha.ua@gmail.com>
2023-04-05 01:26:39 +0200

    update TestSkript 🎢

--- src/test/java/TestSkript.java
+++ src/test/java/TestSkript.java
@@ -1,21 +1,19 @@
 import ua.realalpha.skript.Skript;
-import ua.realalpha.skript.operator.AdditionalOperator;
-import ua.realalpha.skript.operator.EqualsOperator;
-import ua.realalpha.skript.operator.MultiplyOperator;
-import ua.realalpha.skript.operator.SubtractOperation;
+import ua.realalpha.skript.operator.*;
 
 public class TestSkript {
 
     public static void main(String[] args) {
         Skript skript = new Skript();
-        skript.registerOperator(new AdditionalOperator(), new SubtractOperation(), new EqualsOperator(), new MultiplyOperator());
+        skript.registerOperator(new AdditionalOperator(), new SubtractOperator(), new EqualsOperator(), new MultiplyOperator(), new DivideOperator());
 
         long start = System.currentTimeMillis();
 
-        Object result = skript.evaluateExpression("2 + 1+3 - 20 * 2 == 34");
+        Object result = skript.evaluateExpression("2 * 7 / 3.14 + 5 - 2 * 3");
         System.out.println("Result: "+result);
 
         System.out.println("Done ("+(System.currentTimeMillis()-start)+" ms)");
+
     }
-    
+
 }
